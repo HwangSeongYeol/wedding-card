e3eb39b5b0920505633d20620ebfd527656f60dd
Seong Yeol Hwang <seongyeol_hwang@tmax.co.kr>
2024-02-22 22:13:04 +0900

    Test

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+
+console.log('asdf')
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : import.meta.env.VITE_PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
+
   </React.StrictMode>,
 )
