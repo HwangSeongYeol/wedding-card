549386040053d29ff3939712f7440144a1146a0e
Seong Yeol Hwang <seongyeol_hwang@tmax.co.kr>
2024-03-28 22:59:26 +0900

    Updates

---
+++ assets/Home-BhqomPaf.js
@@ -0,0 +1,49 @@
+import{c as i,a,b as e}from"./index-BBmj5Sdh.js";import{p as n}from"./SOK_4935-CFb43LBf.js";import{B as t}from"./Box-4tHCxyWP.js";import"./createTheme-BwqUroVb.js";import"./extendSxProp-DNXoHJQk.js";const r={wrapper:i`
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+  `,mainImageWrapper:i`
+    display: flex;
+    justify-content: center;
+    overflow: hidden;
+    position: relative;
+  `,mainImageFooter:i`
+    position: absolute;
+    top: 0;
+    border-radius: 50%;
+    transform: translateY(-50%);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 180px;
+    width: 270px;
+  `,mainImageDate:i`
+    margin-top: 60px;
+  `,mainImageLocation:i``,img:i`
+    object-fit: cover;
+    /* width: 100vw; */
+    height: 100vh;
+  `,description:i`
+    font-family: "Ownglyph_Dailyokja-Rg";
+    word-break: break-all;
+  `,description1:i`
+    margin: 1rem 0;
+    font-family: "Ownglyph_Dailyokja-Rg";
+    font-size: 2.3rem;
+    word-break: break-all;
+    display: flex;
+    justify-content: center;
+    text-align: center;
+  `,verticalWrapper:i`
+    position: relative;
+    height: 150px;
+  `,vertical:s=>i`
+    border: 2px solid ${s.palette.primary.main};
+    height: 100%;
+    position: absolute;
+    left: 50%;
+    margin-left: -3px;
+    border-radius: 5px;
+    top: 0;
+  `},d=()=>a("div",{className:"pages home",css:r.wrapper,children:[a("div",{css:r.mainImageWrapper,children:[e("img",{css:r.img,src:n}),a(t,{css:r.mainImageFooter,sx:{bgcolor:"background.default"},children:[e("span",{css:r.mainImageDate,children:"2024.05.25 토요일 오후 1시"}),e("span",{css:r.mainImageLocation,children:"전주 엔타워 2층 카시오페아홀"})]})]}),e(t,{css:r.verticalWrapper,children:e("hr",{css:r.vertical})}),a("span",{css:r.description1,children:["황금빛 같은 내일을 향해 ",e("br",{}),"성큼성큼 함께 걸어가는",e("br",{}),"열정으로 가득 찬 우리의 사랑",e("br",{}),"이제 우리 함께",e("br",{}),"서로를 의지하며",e("br",{}),"라떼처럼 달콤하게 살아가요",e("br",{})]}),e(t,{css:r.verticalWrapper,children:e("hr",{css:r.vertical})}),e("span",{css:r.description,children:"황성열 이서라의 결혼식에 초대합니다."}),e("span",{})]});export{d as default};
---
+++ assets/SOK_4935-CFb43LBf.js
@@ -0,0 +1,1 @@
+const s="/wedding-card/assets/SOK_4935-CjEJL-F4.jpg";export{s as p};
